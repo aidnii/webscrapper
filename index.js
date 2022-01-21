fef62ff001c37a841154000bc7ebc24a9e3c848a
cryptichic <syberchief@gmail.com>
2022-01-20 19:11:23 -0600

    successful web scrapper for freecodecamp

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-const PORT = 8000
+const PORT = 8000;
 
-const axios = require('axios')
-const cheerio = require('cheerio')
-const express = require('express')
+const axios = require('axios');
+const cheerio = require('cheerio');
+const express = require('express');
 
-const app = express()
+const app = express();
 
-app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
+//website that i am "webscrapping"
+const url = 'https://www.freecodecamp.org/news/tag/solidity/';
+
+axios(url)
+    .then(response => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const articles = []
+        
+        $('.post-card-title', html).each(function() {
+            const title = $(this).text()
+            const url = $(this).find('a').attr('href')
+            articles.push({
+                title,
+                url
+            })
+        });
+        console.log(articles)
+    }).catch(err => console.log(err))
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
